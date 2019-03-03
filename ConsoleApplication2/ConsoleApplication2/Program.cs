5db804f53815b93e7e318973b485b51e9d3afc40
김용찬 <ycbalor@gmail.com>
2019-03-03 10:50:14 +0900

    상속

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+	public class Dog
+	{
+		public string name { get; set; }
+		public void Jitda()
+		{
+			Console.WriteLine(name + "가 짖다.");
+		}
+
+	}
+
+	public class Pudle : Dog
+	{
+		public void Work() { Console.WriteLine(name + "가 일한다."); }
+	}
+
+	public class Jindo : Dog
+	{
+		public void Work() { Console.WriteLine(name + "가 일한다."); }
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Pudle pudle = new Pudle();
+			pudle.name = "곰둥이";
+			pudle.Jitda();
+			pudle.Work();
+
+			Jindo jindo = new Jindo();
+			jindo.name = "김용찬";
+			jindo.Jitda();
+			jindo.Work();
+		}
+	}
+}
