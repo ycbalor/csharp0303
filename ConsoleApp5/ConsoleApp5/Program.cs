d97e023b5db9494a2fcd96633380f7b642639bde
김용찬 <ycbalor@gmail.com>
2019-03-03 16:20:09 +0900

    깊은복사

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+	class CreditCard
+	{
+		public string name;
+	}
+
+	class Customer : ICloneable
+	{
+		public int age;
+		public CreditCard card;
+
+		public object Clone()
+		{
+			Customer c = new Customer();
+			c.age = this.age;
+			c.card = new CreditCard();
+			c.card.name = this.card.name;
+			return c;
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Customer c1 = new Customer();
+			c1.age = 20;
+			c1.card = new CreditCard();
+			c1.card.name = "비씨카드";
+
+			Customer c2 = (Customer)c1.Clone();
+			c2.card.name = "BC카드";
+
+			Console.WriteLine("c1.card.name = " + c1.card.name);
+			Console.WriteLine("c1.card.name = " + c2.card.name);
+		}
+	}
+}
