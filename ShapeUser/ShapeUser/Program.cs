43e6198656b553412545e64c96a3fbe533e6abb3
김용찬 <ycbalor@gmail.com>
2019-03-03 10:37:47 +0900

    어셈블리 테스트

---
+++ ShapeUser/ShapeUser/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Shapes;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeUser
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Circle c = new Circle(1.0F);
+			Console.WriteLine("Area of Circle(1.0) is {0}", c.Area());
+		}
+	}
+}
