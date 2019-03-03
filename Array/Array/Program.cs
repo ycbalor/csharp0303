a38e4b09d0b2e050bb2224f5837097ce8a52bdfd
김용찬 <ycbalor@gmail.com>
2019-03-03 16:11:17 +0900

    얕은복사

--- Array/Array/Program.cs
+++ Array/Array/Program.cs
@@ -1,40 +1,39 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Array
+namespace ConsoleApplication17
 {
+
+	class CreditCard
+	{
+		public string name;
+	}
+
+	class Customer
+	{
+		public int age;
+		public CreditCard card; // 객체 안에 객체가 포함되어 있음
+
+		public object ShallowCopy()
+		{
+			return this.MemberwiseClone();
+		}
+	}
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			// 배열도 객체라서 힙메모리 영역에 올라감
-			// 힙 영역은 항상 0으로 초기화~
+			Customer c1 = new Customer();
+			c1.age = 20;
+			c1.card = new CreditCard();
+			c1.card.name = "비씨카드";
 
-			// 선언 후 초기화
-			//int[][] arr = new int[2][];
-			//arr[0] = new int[5] { 1, 3, 5, 7, 9 };
-			//arr[1] = new int[4] { 2, 4, 6, 8 };
+			Customer c2 = (Customer)c1.ShallowCopy();
+			c2.card.name = "BC카드";
 
-			// 위와 동일 선언과 동시에 초기화
-			int[][] arr =
-			{
-				new int[5] { 1, 3, 5, 7, 9 },
-				new int[4] { 2, 4, 6, 8 }
-			};
-
-			for (int i = 0; i < arr.Length; i++) // arr의 길이 2
-			{
-				Console.Write("Element({0}):", i);
-				for (int j = 0; j < arr[i].Length; j++) // arr[0]의 길이 5, arr[1]의 길드 4
-				{
-					Console.Write("{0}{1}", arr[i][j], j == (arr[i].Length - 1) ? "" : " ");
-				}
-				Console.WriteLine();
-			}
-			Console.ReadLine();
+			Console.WriteLine("c1.card.name = " + c1.card.name);
+			Console.WriteLine("c2.card.name = " + c2.card.name);
 		}
 	}
 }
