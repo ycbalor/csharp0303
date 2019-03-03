d865bc84cc52abe60a529d16def019d52d93ac42
김용찬 <ycbalor@gmail.com>
2019-03-03 17:41:00 +0900

    컬렉션

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+	class Program
+	{
+		//// 리스트
+		//static void Main(string[] args)
+		//{
+		//	ArrayList onj = new ArrayList();
+		//	// List<string> onj = new List<string>();
+
+		//	onj.Add("Onj");
+		//	onj.Add("OracleJava");
+		//	onj.Add("Community");
+
+		//	Console.WriteLine("onj List");
+		//	Console.WriteLine("Count : {0}", onj.Count);
+		//	Console.WriteLine("Capacity : {0}", onj.Capacity);
+		//	PrintValues(onj);
+
+		//	ArrayList onj2 = (ArrayList)onj.Clone();
+		//	Console.WriteLine("onj2 Values");
+		//	PrintValues(onj2);
+
+		//}
+
+		//public static void PrintValues(IEnumerable myList)
+		//{
+		//	foreach (Object obj in myList)
+		//		Console.WriteLine("{0}", obj);
+		//	Console.WriteLine();
+		//}
+
+		// 해시테이블
+		static void Main(string[] args)
+		{
+			// Hashtable onj = new Hashtable();
+			Dictionary<string, string> onj = new Dictionary<string, string>();
+
+			onj.Add("김길동", "서울");
+			onj.Add("홍길동", "광주");
+			onj.Add("박길동", "부산");
+
+			try
+			{
+				onj.Add("김길동", "서울");
+			}
+			catch
+			{
+				Console.WriteLine("키값 중복..");
+			}
+
+			Console.WriteLine("For Key = \"name\":{0}", onj["홍길동"]);
+			onj["박길동"] = "제주";
+			Console.WriteLine("For Key = \"name\":{0}", onj["박길동"]);
+
+			if (!onj.ContainsKey("최길동"))
+			{
+				onj.Add("최길동", "하와이");
+				Console.WriteLine("Value added for key = \"who\":{0}", onj["박길동"]);
+			}
+			Console.WriteLine();
+
+			// foreach (DictionaryEntry d in onj)
+			foreach(KeyValuePair<string, string> d in onj)
+			{
+				Console.WriteLine("Key = {0}, Value = {1}", d.Key, d.Value);
+			}
+
+			SortedList s = new SortedList(onj); // 해시 테이블을 정렬하기 위해 Sorted 리스트에 넣는다.
+
+			foreach (DictionaryEntry d in s)
+			{
+				Console.WriteLine("Key = {0}, Value = {1}", d.Key, d.Value);
+			}
+		}
+	}
+}
---
+++ ConsoleApplication6/ConsoleApplication6/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApplication6
+{
+	//class Stack1
+	//{
+	//	int top = 0;
+	//	int[] ar = new int[10];
+
+	//	public void Push(int obj)
+	//	{
+	//		ar[top] = obj;
+	//		top++;
+	//	}
+
+	//	public int Pop()
+	//	{
+	//		top--;
+	//		return ar[top];
+	//	}
+	//}
+
+	//class Stack2
+	//{
+	//	int top = 0;
+	//	string[] ar = new string[10];
+
+	//	public void Push(string obj)
+	//	{
+	//		ar[top] = obj;
+	//		top++;
+	//	}
+
+	//	public string Pop()
+	//	{
+	//		top--;
+	//		return ar[top];
+	//	}
+	//}
+
+	class Stack3<T>
+	{
+		int top = 0;
+		T[] ar = new T[10];
+
+		public void Push(T obj)
+		{
+			ar[top] = obj;
+			top++;
+		}
+
+		public T Pop()
+		{
+			top--;
+			return ar[top];
+		}
+	}
+
+
+	class StackTest
+	{
+		static void Main()
+		{
+			Stack3<int> s3 = new Stack3<int>();
+
+			s3.Push(1);
+			s3.Push(2);
+			s3.Push(3);
+			Console.WriteLine(s3.Pop());
+			Console.WriteLine(s3.Pop());
+			Console.WriteLine(s3.Pop());
+
+			Stack3<string> s4 = new Stack3<string>();
+			s4.Push("KOREA");
+			s4.Push("대한민국");
+			s4.Push("서울");
+
+			Console.WriteLine(s4.Pop());
+			Console.WriteLine(s4.Pop());
+			Console.WriteLine(s4.Pop());
+
+			//Stack1 s1 = new Stack1();
+
+			//s1.Push(1);
+			//s1.Push(2);
+			//s1.Push(3);
+			//Console.WriteLine(s1.Pop());
+			//Console.WriteLine(s1.Pop());
+			//Console.WriteLine(s1.Pop());
+
+			//Stack2 s2 = new Stack2();
+
+			//s2.Push("KOREA");
+			//s2.Push("대한민국");
+			//s2.Push("서울");
+
+			//Console.WriteLine(s2.Pop());
+			//Console.WriteLine(s2.Pop());
+			//Console.WriteLine(s2.Pop());
+		}
+	}
+}
